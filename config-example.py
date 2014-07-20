1851d22b96b4411f586b1f34e9791b31f90a0ce9
Peter Urbak <peter@dragonwasrobot.com>
2014-07-21 00:25:17 +0200

    Implemented a fully functional login system.

--- config-example.py
+++ config-example.py
@@ -12,4 +12,4 @@ OPENID_PROVIDERS = [
   { 'name': 'Yahoo', 'url': 'https://me.yahoo.com' },
   { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
   { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
-  { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' } ]
+  { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
