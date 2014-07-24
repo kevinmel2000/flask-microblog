0df801d56704e664c4fec1eefe29fdf4798f2be1
Peter Urbak <peter@dragonwasrobot.com>
2014-07-24 23:48:24 +0200

    Added logging and unit testing.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ flask-microblog
 ===================
 
 A microblog written in Python using the Flask framework.
+
+# Debugging
+
+Start the server by executing: `./run.py` for debug mode and `./runp.py` for
+production mode.
+
+Run unit tests by executing: `/tests.py`.
+
+Running a fake mail server: `python -m smtpd -n -c DebuggingServer localhost:25`.
--- config-example.py
+++ config-example.py
@@ -1,11 +1,20 @@
 import os
+
+# Misc
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+# SQLAlchemy
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
+# Forms
+
 CSRF_ENABLED = True
 SECRET_KEY = '<REPLACE WITH REAL VALUE>'
+
+# OpenID
 
 OPENID_PROVIDERS = [
   { 'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id' },
@@ -13,3 +22,13 @@ OPENID_PROVIDERS = [
   { 'name': 'AOL', 'url': 'http://openid.aol.com/<username>' },
   { 'name': 'Flickr', 'url': 'http://www.flickr.com/<username>' },
   { 'name': 'MyOpenID', 'url': 'https://www.myopenid.com' }]
+
+# Mail server settings
+
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+# administrator list
+ADMINS = ['tolowercase@gmail.com']
